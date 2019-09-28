643b663f17c09cf311523b7523633daa24678a63
852-pan0s <panospoonforschool@gmail.com>
2019-09-29 00:20:47 +0800

    Login function is done

--- js/login.js
+++ js/login.js
@@ -1,19 +1,8 @@
 // the file needs to run with jQuery.
-// import { Login } from "login";
-
-var login = (username, password) => {
+const get = (username, password) => {
     return $.getJSON("../db/users.json").then((data) => {
-        $.each(data, (type, values) => {
-            $.each(values, (id, userInfo) => {
-                if (username === userInfo.username && password === userInfo.password) {
-                    console.log("login!!")
-                    return true;
-                };
-            });
-        });
-        return false;
-    }
-    );
+        return data;
+    });
 };
 
-export { login }
+export { get }
